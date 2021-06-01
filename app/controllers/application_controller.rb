49a05d028ab921b2601ecb5644c393d695c59495
Sarah <sarahlchavez@gmail.com>
2021-06-01 11:21:33 -0700

    commit tree 2e12f1dbbacfb4e053bfc0f732b00a1c204826aa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'super_hero'
+    end
 
+    post '/teams' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
+      members.each do |member|
+        SuperHero.new({name: member[:name], power: member[:power], bio: member[:bio]})
+      end
+      @super_heroes = SuperHero.all
+      erb :'team'
+    end
 end
+  
